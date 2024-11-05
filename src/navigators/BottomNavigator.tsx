4dec67338f75d58540cd3ef81c47c03489854711
lovhurt <alicandagidir@hotmail.com>
2024-11-05 21:39:02 +0300

    add standartnavigator

--- src/navigators/BottomNavigator.tsx
+++ src/navigators/BottomNavigator.tsx
@@ -8,6 +8,7 @@ import GiftScreen from "../screens/GiftScreen";
 import { Feather, FontAwesome, Ionicons } from "@expo/vector-icons";
 import CustomBarNavigation from "./CustomBarNavigation";
 import HomeNavigator from "./header/HomeNavigator";
+import StandartNavigator from "./header/StandartNavigator";
 
 const Tab = createBottomTabNavigator();
 
@@ -32,13 +33,15 @@ const BottomNavigator = () => {
       />
       <Tab.Screen
         name="Search"
-        component={SearchScreen}
         options={{
           tabBarIcon: ({ color }) => (
             <Feather name="search" size={24} color={color} />
           ),
         }}
-      />
+      >
+        {()=><StandartNavigator component={SearchScreen} name="search" textTitle="Search"/>}
+      </Tab.Screen>
+      
       <Tab.Screen
         name="CustomBar"
         component={CustomBarNavigation}
@@ -50,22 +53,24 @@ const BottomNavigator = () => {
       />
       <Tab.Screen
         name="Profile"
-        component={ProfileScreen}
         options={{
           tabBarIcon: ({ color }) => (
             <FontAwesome name="user" size={24} color={color} />
           ),
         }}
-      />
+        >
+        {()=><StandartNavigator component={ProfileScreen} name="profile" textTitle="Profile"/>}
+      </Tab.Screen>
       <Tab.Screen
         name="Gift"
-        component={GiftScreen}
         options={{
           tabBarIcon: ({ color }) => (
             <Ionicons name="gift-sharp" size={24} color={color} />
           ),
         }}
-      />
+        >
+        {()=><StandartNavigator component={GiftScreen} name="gift" logo textTitle="Gift"/>}
+      </Tab.Screen>
     </Tab.Navigator>
   );
 };
---
+++ src/navigators/header/StandartNavigator.tsx
@@ -0,0 +1,73 @@
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "../../screens/HomeScreen";
+import { Entypo } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+
+interface StandartNavigatorProps {
+  component: React.ComponentType<any>;
+  name: string;
+  logo?: boolean;
+  textTitle?: string;
+}
+const Stack = createStackNavigator();
+
+export default function StandartNavigator({
+  component,
+  name,
+  logo,
+  textTitle,
+}: StandartNavigatorProps) {
+  const navigation = useNavigation();
+
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name={name}
+        component={component}
+        options={{
+          headerStyle: { backgroundColor: "#5c3ebc" },
+          headerTitleAlign: "center",
+          headerLeft: () => (
+            <TouchableOpacity
+              className="m-2"
+              onPress={() => navigation.goBack()}
+            >
+              <Entypo name="chevron-left" size={24} color="white" />
+            </TouchableOpacity>
+          ),
+          headerTitle: () => (
+            <View>
+              {logo ? (
+                <Image
+                  resizeMode="contain"
+                  className="w-16 h-8"
+                  source={require("../../../assets/logo.png")}
+                />
+              ) : (
+                <Text className="text-white font-semibold text-lg">
+                  {textTitle}
+                </Text>
+              )}
+            </View>
+          ),
+          headerRight: () => (
+            <TouchableOpacity
+              onPress={() => navigation.navigate("Home")}
+              className="bg-white p-2 m-2 h-8 flex-row rounded-xl items-center justify-center"
+            >
+              <Image
+                source={require("../../../assets/cart.png")}
+                className="w-5 h-7 p-2"
+              />
+              <View className="flex flex-col bg-[#f3effe] h-7 items-center justify-center">
+                <Text>{"\u20BA"}555</Text>
+              </View>
+            </TouchableOpacity>
+          ),
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
